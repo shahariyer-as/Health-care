993da527056f6c4ab0551845eaa4c7cf7e0b6b40
shahariyer.as <shahariyer.as@gmail.com>
2022-08-29 19:11:20 +0600

    header services edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home/Home/Home';
-import Footer from './Pages/Shared/Footer/Footer';
-import Header from './Pages/Shared/Header/Header';
-import Login from './Pages/Login/Login';
-import Signup from './Pages/Signup/Signup';
-import NotFound from './Pages/NotFound/NotFound';
-import About from './Pages/About/About';
-import Specialist from './Pages/Specialist/Specialist';
-import Contact from './Pages/Contact/Contact';
-import ServiceDetail from './Pages/Home/ServiceDetail/ServiceDetail';
-import AuthProvider from './context/AuthProvider';
-import PrivateRoute from './Pages/PrivateRoute/PrivateRoute';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import NotFound from "./Pages/NotFound/NotFound";
+import About from "./Pages/About/About";
+import Specialist from "./Pages/Specialist/Specialist";
+import Contact from "./Pages/Contact/Contact";
+import ServiceDetail from "./Pages/Home/ServiceDetail/ServiceDetail";
+import AuthProvider from "./context/AuthProvider";
+import PrivateRoute from "./Pages/PrivateRoute/PrivateRoute";
+// import Service from "./Pages/Home/Service/Service";
+import Services from "./Pages/Home/Services/Services";
 
 function App() {
   return (
@@ -39,6 +41,9 @@ function App() {
 
             <Route path="/specialist">
               <Specialist></Specialist>
+            </Route>
+            <Route path="/services">
+              <Services></Services>
             </Route>
 
             <Route path="/contact">
